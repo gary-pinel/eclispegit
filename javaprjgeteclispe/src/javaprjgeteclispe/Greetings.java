4a64b18a18ddf1cf4fd6c47315f89dfbaa3f2870
gpinel <gpinel@LHTU05CG8295YHD.ms.ds.uhc.com>
2020-02-16 12:29:03 -0500

    This is a change as part of the upstream push

---
+++ javaprjgeteclispe/src/javaprjgeteclispe/Greetings.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package javaprjgeteclispe;
+
+/**
+ * @author gpinel Jenkins Lab1 Part 1
+ *
+ */
+public class Greetings {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Greetings LYIT Student"); 
+		System.out.println("Goodbye LYIT Student");
+	}
+
+}
